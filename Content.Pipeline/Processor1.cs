e18943de8849d0b3771c9d66fda763a7fc59ee2a
Nathan Bean <nhbean@ksu.edu>
2021-03-02 17:02:36 -0600

    Initial Commit

---
+++ Content.Pipeline/Processor1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+
+using TInput = System.String;
+using TOutput = System.String;
+
+namespace Content.Pipeline
+{
+    [ContentProcessor(DisplayName = "Processor1")]
+    class Processor1 : ContentProcessor<TInput, TOutput>
+    {
+        public override TOutput Process(TInput input, ContentProcessorContext context)
+        {
+            return default(TOutput);
+        }
+    }
+}
